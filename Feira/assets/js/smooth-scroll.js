bac3fd10ab7a499e7873765fbe999b2d8b8518eb
Santozzz <107252394+Santozzz@users.noreply.github.com>
2023-10-06 11:07:39 -0300

    Add files via upload

--- Feira/assets/js/smooth-scroll.js
+++ Feira/assets/js/smooth-scroll.js
@@ -3,3 +3,9 @@ const botao = document.querySelector('.btn-vermais');
 botao.addEventListener('click', () =>{
     window.scroll({top: window.innerHeight, behavior: "smooth"})
 })
+
+const inicio = document.querySelector('.navbar-item');
+
+inicio.addEventListener('click', () =>{
+    window.scroll({top: window.innerHeight, behavior: "smooth"})
+}) 
